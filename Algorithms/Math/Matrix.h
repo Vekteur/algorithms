706a86dc631871f7ea8ad744ebce0d6d772be15d
Vekteur <v.dheur@hotmail.be>
2019-02-19 18:26:05 +0100

    Improve matrix

--- Algorithms/Math/Matrix.h
+++ Algorithms/Math/Matrix.h
@@ -21,6 +21,43 @@ public:
 		return n;
 	}
 
+	Matrix<T> operator+(const Matrix<T>& m) const {
+		Matrix<T> r(size());
+		for (int i = 0; i < size(); ++i)
+			for (int j = 0; j < size(); ++j)
+				r(i, j) = (*this)(i, j) + m(i, j);
+		return r;
+	}
+
+	Matrix<T> operator-() const {
+		Matrix<T> r(size());
+		for (int i = 0; i < size(); ++i)
+			for (int j = 0; j < size(); ++j)
+				r(i, j) = -(*this)(i, j);
+		return r;
+	}
+
+	Matrix<T> operator-(const Matrix<T>& m) const {
+		return (*this) + -m;
+	}
+
+	bool operator==(const Matrix<T>& m) const {
+		for (int i = 0; i < size(); ++i)
+			for (int j = 0; j < size(); ++j)
+				if ((*this)(i, j) != m(i, j))
+					return false;
+		return true;
+	}
+
+	Matrix<T> operator*(const Matrix<T>& m) const {
+		Matrix<T> r(size());
+		for (int i = 0; i < size(); ++i)
+			for (int j = 0; j < size(); ++j)
+				for (int k = 0; k < size(); ++k)
+					r(i, j) += (*this)(i, k) * m(k, j);
+		return r;
+	}
+
 private:
 	int n;
 	std::vector<T> vec;
--- Algorithms/Math/MatrixPower.h
+++ Algorithms/Math/MatrixPower.h
@@ -3,43 +3,6 @@
 #include "Matrix.h"
 
 namespace {
-Matrix<int> operator+(const Matrix<int>& a, const Matrix<int>& b) {
-	Matrix<int> r{ a.size() };
-	for (int i = 0; i < a.size(); ++i)
-		for (int j = 0; j < a.size(); ++j)
-			r(i, j) = a(i, j) + b(i, j);
-	return r;
-}
-
-Matrix<int> operator-(const Matrix<int>& a) {
-	Matrix<int> r{ a.size() };
-	for (int i = 0; i < a.size(); ++i)
-		for (int j = 0; j < a.size(); ++j)
-			r(i, j) = -a(i, j);
-	return r;
-}
-
-Matrix<int> operator-(const Matrix<int>& a, const Matrix<int>& b) {
-	return a + -b;
-}
-
-bool operator==(const Matrix<int>& a, const Matrix<int>& b) {
-	for (int i = 0; i < a.size(); ++i)
-		for (int j = 0; j < a.size(); ++j)
-			if (a(i, j) != b(i, j))
-				return false;
-	return true;
-}
-
-Matrix<int> operator*(const Matrix<int>& a, const Matrix<int>& b) {
-	Matrix<int> r{ a.size() };
-	for (int i = 0; i < a.size(); ++i)
-		for (int j = 0; j < a.size(); ++j)
-			for (int k = 0; k < a.size(); ++k)
-				r(i, j) += a(i, k) * b(k, j);
-	return r;
-}
-
 Matrix<int> identityMatrix(int n) {
 	Matrix<int> r{ n };
 	for (int i = 0; i < n; ++i)
